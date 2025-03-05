fe57829e80c0c3eed675a3c923e669f74de604bf
lucassantss <lucas.santosrod@hotmail.com>
2025-03-05 11:12:38 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import logo from './imagens/logo.svg';
+import Logo from './components/Logo';
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Lucas</strong> Books</p>
-        </div>
+
       </header>
     </div>
   );
